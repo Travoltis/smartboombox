a9f2826e8e0d973ecdd93c16ea53e0446ea0e91a
Marco Ris <ris@contria.com>
2020-04-08 17:10:45 +0200

    feat(vue): Add package version component

---
+++ src/components/PackageVersion.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="display: inline-block;">v{{version}}</div>
+</template>
+
+<script>
+import {version} from '../../package.json';
+
+export default {
+    name: 'PackageInfo',
+
+    data() {
+        return {
+        version
+        };
+    }
+}
+</script>
