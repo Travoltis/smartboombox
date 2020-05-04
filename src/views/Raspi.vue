74db1629710044dd756f9cd82523566cc842b402
Marco Ris <ris@contria.com>
2020-05-04 19:43:40 +0200

    Add icons and functionality

--- src/views/Raspi.vue
+++ src/views/Raspi.vue
@@ -1,6 +1,32 @@
 <template>
     <div class="container">
         <h1>Raspi</h1>
-        <p>Funktionen</p>
+        <v-btn v-on:click="shutdown" depressed class="pink white--text">
+            <v-icon>{{shutdownIcon}}</v-icon>
+            </v-btn>
+            <v-btn v-on:click="restart" depressed class="pink white--text">
+            <v-icon>{{restartIcon}}</v-icon>
+        </v-btn>
     </div>
 </template>
+
+<script>
+// icons from https://materialdesignicons.com/ with prefix mdi
+import { mdiPower, mdiRestart } from '@mdi/js'
+
+export default {
+    name: "Raspi",
+    data: () => ({
+        shutdownIcon: mdiPower,
+        restartIcon: mdiRestart,
+    }),
+    methods: {
+        shutdown: () => {
+            console.log('shutting down...');
+        },
+        restart: () => {
+            console.log('restarting...');
+        },
+    }
+}
+</script>
