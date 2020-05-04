733035214e8bfb153f1ae1cbadb43ec044e892b6
Marco Ris <ris@contria.com>
2020-05-04 19:43:24 +0200

    Add temporarily functions

--- src/views/Wifi.vue
+++ src/views/Wifi.vue
@@ -2,11 +2,19 @@
     <div class="container">
         <h1>WLAN</h1>
         <p>Tet</p>
+        <v-btn v-on:click="test" depressed class="pink white--text">
+          <v-icon>email</v-icon>
+        </v-btn>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Wifi"
+    name: "Wifi",
+    methods: {
+        test: () => {
+            console.log('jo');
+        }
+    }
 }
 </script>
