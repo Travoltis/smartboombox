5b0d0f2166aa523c312b582eaccf220cef939ec6
Marco Ris <ris@contria.com>
2020-05-04 19:41:16 +0200

    Add global styling and outsource the navigation

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,24 @@
 <template>
   <v-app>
-    <div id="nav" class="navigation">
-      <router-link to="/">Home</router-link>
-      <router-link to="/airplay">Airplay</router-link>
-      <router-link to="/accesspoint">Accesspoint</router-link>
-      <router-link to="/bluetooth">Bluetooth</router-link>
-      <router-link to="/raspi">Raspi</router-link>
-      <router-link to="/wifi">WLAN</router-link>
-      <router-link to="/settings">Settings</router-link>
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view/>
+    <Player />
   </v-app>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+import Player from './components/Player'
 
 export default {
-  name: 'App',
-  data: () => ({
-    //
-  }),
+    name: 'App',
+    components: {
+        Navbar,
+        Player
+    }
 };
 </script>
 
 <style lang="scss">
-
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-}
-
-body {
-    font: 1.2rem/1.6 Arial, Helvetica, sans-serif;
-}
-
-h1 {
-    padding-bottom: 20px;
-    text-align: center;
-}
-
-.navigation {
-    border-bottom: 1px solid gray;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, .75);
-    display: flex;
-    justify-content: center;
-    line-height: 60px;
-    text-align: center;
-    position: fixed;
-    width: 100%;
-    z-index: 100 !important;
-
-    a {
-        border: 1px solid gray;
-        border-bottom: none;
-        border-top: none;
-        min-width: 150px;
-        text-decoration: none;
-
-        &:hover,
-        &:active,
-        &:focus,
-        &.router-link-exact-active.router-link-active {
-            background-color: #eee;
-        }
-
-        &+a {
-            border-left: none;
-        }
-    }
-}
-
-.container {
-    height: calc(100vh - 65px);
-    min-width: 100%;
-    margin-top: 65px;
-    padding: 20px !important;
-    overflow: hidden;
-    overflow-y: auto;
-    z-index: 0 !important;
-
-    &:after {
-        background-image: url('./assets/logo.png');
-        background-position: center center;
-        background-repeat: no-repeat;
-        background-size: 35%;
-        background-attachment: fixed;
-        content: '';
-        display: block;
-        height: 75%;
-        width: 100%;
-        opacity: .5;
-        position: absolute;
-        top: 150px;
-        z-index: -1;
-    }
-}
+@import './scss/style.scss';
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="nav" class="navigation">
+        <router-link to="/"><v-icon>{{homeIcon}}</v-icon></router-link>
+        <router-link to="/bluetooth"><v-icon>{{bluetoothIcon}}</v-icon></router-link>
+        <router-link to="/wifi"><v-icon>{{wifiIcon}}</v-icon></router-link>
+        <router-link to="/raspi"><v-icon>{{raspiIcon}}</v-icon></router-link>
+        <router-link to="/settings"><v-icon>{{settingsIcon}}</v-icon></router-link>
+        <router-link to="/about"><v-icon>{{aboutIcon}}</v-icon></router-link>
+    </div>
+</template>
+
+<script>
+// icons from https://materialdesignicons.com/ with prefix mdi
+import { mdiHome, mdiCog, mdiBluetooth, mdiWifi, mdiRaspberryPi, mdiInformation } from '@mdi/js'
+
+export default {
+    name: 'Navbar',
+    data: () => ({
+        homeIcon: mdiHome,
+        bluetoothIcon: mdiBluetooth,
+        wifiIcon: mdiWifi,
+        raspiIcon: mdiRaspberryPi,
+        settingsIcon: mdiCog,
+        aboutIcon: mdiInformation
+    })
+}
+</script>
+
+<style lang="scss">
+@import '../scss/style.scss';
+
+.navigation {
+    box-shadow: 0 5px 10px $shadowColor;
+    display: flex;
+    line-height: 60px;
+    position: fixed;
+    text-align: center;
+    width: 100%;
+    z-index: 100;
+
+    a {
+        flex-grow: 1;
+        text-decoration: none;
+
+        &:hover,
+        &:active,
+        &:focus,
+        &.router-link-exact-active.router-link-active {
+            background-color: $whitesmokeColor;
+
+            svg {
+                fill: $blackColor;
+            }
+        }
+
+        &+a {
+            border-left: 1px solid $grayColor;
+        }
+    }
+}
+</style>
