f083e59f8d5bcca1972064dbc2d4d9d63bb99faf
Marco Ris <ris@contria.com>
2020-05-05 18:24:46 +0200

    Add bluetooth request device chooser

--- src/views/Bluetooth.vue
+++ src/views/Bluetooth.vue
@@ -1,11 +1,28 @@
 <template>
     <div class="container">
         <h1>Bluetooth</h1>
-        <p>Verbindung zu Bluetooth
-        Bluetooth aktivieren
+        <p>Verbindung zu Bluetooth aktivieren
         raspberrypi auswählen
         Pairing Request mit OK bestätigen
         Musik abspielen
         Enjoy it</p>
+        <v-btn id="button" @click="requestDeviceChooser" depressed class="pink white--text"><v-icon>Koppeln</v-icon></v-btn>
     </div>
 </template>
+
+<script>
+const requestDeviceChooser = () => {
+    // Call navigator.bluetooth.requestDevice
+    navigator.bluetooth.requestDevice({ filters: [{ services: ['device_information'] }] })
+  .then(device => { console.log(device); })
+  .catch(error => { console.log(error); });
+}
+
+export default {
+    data() {
+        return {
+            requestDeviceChooser
+        }
+    }
+}
+</script>
