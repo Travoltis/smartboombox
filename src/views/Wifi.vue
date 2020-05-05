4728bbf06e1cae98d004d64561286c472220967d
Marco Ris <ris@contria.com>
2020-05-05 18:24:19 +0200

    Add check wifi connection

--- src/views/Wifi.vue
+++ src/views/Wifi.vue
@@ -1,20 +1,41 @@
 <template>
     <div class="container">
-        <h1>WLAN</h1>
-        <p>Tet</p>
-        <v-btn v-on:click="test" depressed class="pink white--text">
-          <v-icon>email</v-icon>
-        </v-btn>
+        <h1>Wifi</h1>
+        <p id="msg" class="pa-1 text-center accent-1"></p>
+        <v-btn id="btnWifi" v-on:click="test" depressed class="pink white--text hidden ma-auto">Wifi aktivieren</v-btn>
     </div>
 </template>
 
 <script>
+// const msg = document.querySelector('#msg');
+
 export default {
-    name: "Wifi",
     methods: {
         test: () => {
-            console.log('jo');
+            console.log('connecting...');
+        },
+        checkWifi: function() {
+            let msg = document.querySelector('#msg');
+            let btnWifi = document.querySelector('#btnWifi');
+            if (msg && btnWifi) {
+                if (navigator.onLine === true) {
+                    msg.classList.remove('red');
+                    msg.classList.add('green');
+                    btnWifi.classList.add('hidden');
+                    btnWifi.classList.remove('d-block');
+                    msg.innerText = 'Smartboombox ist online.';
+                } else {
+                    msg.classList.add('red');
+                    msg.classList.remove('green');
+                    btnWifi.classList.remove('hidden');
+                    btnWifi.classList.add('d-block');
+                    msg.innerText = 'Smartboombox ist offline.';
+                }
+            }
         }
+    },
+    created() {
+        setInterval(this.checkWifi, 1000);
     }
 }
 </script>
